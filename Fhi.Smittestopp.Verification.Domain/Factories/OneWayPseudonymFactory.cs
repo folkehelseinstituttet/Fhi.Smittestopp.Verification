375e41c3772ae78876b97e777cf7578ec97ddd64
Sindre Møgster Braaten <sindre.mb@gmail.com>
2020-11-04 16:46:03 +0100

    Changed hashing algorithm from HMAC to PBKDF2 to resolve fix issue #9

--- Fhi.Smittestopp.Verification.Domain/Factories/OneWayPseudonymFactory.cs
+++ Fhi.Smittestopp.Verification.Domain/Factories/OneWayPseudonymFactory.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
 using Fhi.Smittestopp.Verification.Domain.Interfaces;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.Extensions.Options;
 
 namespace Fhi.Smittestopp.Verification.Domain.Factories
@@ -17,11 +16,17 @@ namespace Fhi.Smittestopp.Verification.Domain.Factories
 
         public string Create(string original)
         {
-            using (var hmacsha256 = new HMACSHA256(_config.KeyBytes))
-            {
-                var hash = hmacsha256.ComputeHash(Encoding.UTF8.GetBytes(original));
-                return Convert.ToBase64String(hash);
-            }
+            return Hash(original, _config.KeyBytes, KeyDerivationPrf.HMACSHA256, _config.NumIterations, _config.NumBytes);
+        }
+
+        /// <summary>
+        /// Simplified version of https://github.com/dotnet/aspnetcore/blob/9428ac6f4c2103227c46a4da23b4de9cb780dc08/src/Identity/Extensions.Core/src/PasswordHasher.cs#L141
+        /// Uses a fixed salt, as we need reproducible results
+        /// </summary>
+        private string Hash(string original, byte[] salt, KeyDerivationPrf prf, int iterCount, int numBytesRequested)
+        {
+            var hashBytes = KeyDerivation.Pbkdf2(original, salt, prf, iterCount, numBytesRequested);
+            return Convert.ToBase64String(hashBytes);
         }
 
         public class Config
@@ -29,10 +34,12 @@ namespace Fhi.Smittestopp.Verification.Domain.Factories
             public string Key
             {
                 get => Convert.ToBase64String(KeyBytes ?? new byte[0]);
-                set => KeyBytes = Encoding.UTF8.GetBytes(value);
+                set => KeyBytes = Convert.FromBase64String(value);
             }
 
             public byte[] KeyBytes { get; set; }
+            public int NumIterations { get; set; } = 10000;
+            public int NumBytes { get; set; } = 256 / 8;
         }
     }
 }

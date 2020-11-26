55857354873931228f34cd048350eab33d3e3eb2
Sindre Møgster Braaten <sindre.mb@gmail.com>
2020-11-26 19:13:53 +0100

    Added data protection with keys persisted to existing db-context

--- Fhi.Smittestopp.Verification.Persistence/VerificationDbContext.cs
+++ Fhi.Smittestopp.Verification.Persistence/VerificationDbContext.cs
@@ -1,14 +1,16 @@
 ﻿using Fhi.Smittestopp.Verification.Persistence.Entities;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fhi.Smittestopp.Verification.Persistence
 {
-    public class VerificationDbContext : DbContext
+    public class VerificationDbContext : DbContext, IDataProtectionKeyContext
     {
         public VerificationDbContext(DbContextOptions<VerificationDbContext> options) : base(options)
         {
         }
 
         public DbSet<VerificationRecordEntity> VerificationRecords { get; set; }
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
     }
 }

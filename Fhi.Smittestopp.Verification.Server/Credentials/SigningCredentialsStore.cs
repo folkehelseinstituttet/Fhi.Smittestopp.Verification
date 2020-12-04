a663cf887cf8157cb96f4e935ea9d7f185a9a890
Sindre Møgster Braaten <sindre.mb@gmail.com>
2020-12-04 12:55:33 +0100

    Added additional config options to SigningCredentialsStore to make it possible to perform signing cert rollover with local cert store (and similarly in Azure Key Vault rollover across cert "names" in addition to already supported cert versions)

--- Fhi.Smittestopp.Verification.Server/Credentials/SigningCredentialsStore.cs
+++ Fhi.Smittestopp.Verification.Server/Credentials/SigningCredentialsStore.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Fhi.Smittestopp.Verification.Domain.Interfaces;
+using Fhi.Smittestopp.Verification.Domain.Models;
 using IdentityServer4.Models;
 using IdentityServer4.Stores;
 using Microsoft.Extensions.Caching.Memory;
@@ -62,15 +63,21 @@ namespace Fhi.Smittestopp.Verification.Server.Credentials
         private async Task<(SigningCredentials active, IEnumerable<SecurityKeyInfo> secondary)> RefreshCacheAsync(ICacheEntry cache)
         {
             cache.AbsoluteExpiration = DateTime.Now.AddDays(1);
-            var enabledCerts = await _certificateLocator.GetAllEnabledCertificateVersionsAsync(_config.Certificate);
+            var enabledCerts = await _certificateLocator.GetAllEnabledCertificateVersionsAsync(_config.Signing);
 
             var rolloverTime = DateTime.UtcNow - _config.KeyRolloverDuration;
             var activeSigningCredentials = enabledCerts.FirstOrNone(x => x.Timestamp < rolloverTime)
                 .Else(enabledCerts.FirstOrNone)
                 .Map(x => new SigningCredentials(new X509SecurityKey(x.Certificate), SigningAlgorithm))
-                .ValueOr(() => throw new Exception("Unable to locate signing certificate: " + _config.Certificate));
+                .ValueOr(() => throw new Exception("Unable to locate signing certificate: " + _config.Signing));
 
-            var enabledValidationKeys = enabledCerts.Select(x => new SecurityKeyInfo
+            var additionalValidationCerts = new List<CertificateVersion>();
+            foreach (var addValidationCert in _config.AdditionalValidation)
+            {
+                additionalValidationCerts.AddRange(
+                    await _certificateLocator.GetAllEnabledCertificateVersionsAsync(addValidationCert));
+            }
+            var enabledValidationKeys = enabledCerts.Concat(additionalValidationCerts).Select(x => new SecurityKeyInfo
             {
                 Key = new X509SecurityKey(x.Certificate),
                 SigningAlgorithm = SigningAlgorithm
@@ -81,8 +88,20 @@ namespace Fhi.Smittestopp.Verification.Server.Credentials
 
         public class Config
         {
+            /// <summary>
+            /// Only used for certs loaded from AzureKeyVault when multiple versions are available. Sets duration a new cert version must have been available for before it takes over as the new signing cert.
+            /// </summary>
             public TimeSpan KeyRolloverDuration { get; set; } = TimeSpan.FromHours(24);
-            public string Certificate { get; set; }
+            /// <summary>
+            /// The certificate used for signing issued tokens.
+            /// Azure Key Vault: The name of the certificate
+            /// Local store: The thumbprint of the certificate
+            /// </summary>
+            public string Signing { get; set; }
+            /// <summary>
+            /// Any certificates valid for token signature validation other than the one provided for "Signing". Mostly intented for use with managing cert rollover with the local cert store
+            /// </summary>
+            public string[] AdditionalValidation { get; set; }
         }
     }
 }

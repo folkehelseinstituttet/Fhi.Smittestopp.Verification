cc95d1e309ec373f9f35a8ee15c17b94a87f1b84
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-12-08 18:55:41 +0100

    Add anonymous-token protocol tests to verify expected values in token generation request and response

--- Fhi.Smittestopp.Verification.Tests/Domain/AnonymousTokens/IssueAnonymousTokenTests.cs
+++ Fhi.Smittestopp.Verification.Tests/Domain/AnonymousTokens/IssueAnonymousTokenTests.cs
@@ -1,20 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using AnonymousTokens.Client.Protocol;
+using AnonymousTokens.Core.Services;
+using AnonymousTokens.Core.Services.InMemory;
+using AnonymousTokens.Server.Protocol;
+
 using Fhi.Smittestopp.Verification.Domain.AnonymousTokens;
 using Fhi.Smittestopp.Verification.Domain.Dtos;
 using Fhi.Smittestopp.Verification.Domain.Interfaces;
 using Fhi.Smittestopp.Verification.Domain.Models;
+
 using FluentAssertions;
 using FluentAssertions.Execution;
+
 using Microsoft.Extensions.Options;
+
 using Moq;
 using Moq.AutoMock;
+
 using NUnit.Framework;
+
 using Optional.Unsafe;
+
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Crypto.EC;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Utilities.Encoders;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Fhi.Smittestopp.Verification.Tests.Domain.AnonymousTokens
 {
@@ -108,19 +125,44 @@ namespace Fhi.Smittestopp.Verification.Tests.Domain.AnonymousTokens
                 .Returns<string>(x => Task.FromResult(Enumerable.Empty<AnonymousTokenIssueRecord>()));
 
             automocker
-                .Setup<IAnonymousTokensCertLocator, Task<X509Certificate2>>(x => x.GetCertificateAsync())
-                .ReturnsAsync(new X509Certificate2()); // TODO: replace with certificate expected in test
+                .Setup<IPrivateKeyStore, Task<BigInteger>>(x => x.GetAsync()).Returns(new InMemoryPrivateKeyStore().GetAsync());
+
+            automocker
+                .Setup<IPublicKeyStore, Task<ECPublicKeyParameters>>(x => x.GetAsync()).Returns(new InMemoryPublicKeyStore().GetAsync());
+
+            // Initiate AnonymousTokens protocol
+            var initiator = new Initiator();
+            var tokenGenerator = new TokenGenerator();
+            var tokenVerifier = new TokenVerifier(new InMemorySeedStore());
+
+            var ecParameters = CustomNamedCurves.GetByOid(X9ObjectIdentifiers.Prime256v1);
+            var publicKeyStore = new InMemoryPublicKeyStore();
+            var publicKey = publicKeyStore.GetAsync().GetAwaiter().GetResult();
+
+            var privateKeyStore = new InMemoryPrivateKeyStore();
+            var privateKey = privateKeyStore.GetAsync().GetAwaiter().GetResult();
+
+            var init = initiator.Initiate(ecParameters.Curve);
+            var t = init.t;
+            var r = init.r;
+            var P = init.P;
+
+            var (Q, proofC, proofZ) = tokenGenerator.GenerateToken(privateKey, publicKey.Q, ecParameters, P);
+
+            automocker
+                .Setup<ITokenGenerator, (ECPoint Q, BigInteger c, BigInteger z)>(x => x.GenerateToken(privateKey, publicKey.Q, ecParameters, P))
+                .Returns((Q, proofC, proofZ));
 
             var target = automocker.CreateInstance<IssueAnonymousToken.Handler>();
 
-            //Act
+            //Act           
             var result = await target.Handle(new IssueAnonymousToken.Command
             {
                 JwtTokenId = "token-a",
                 JwtTokenExpiry = DateTime.Now.AddMinutes(10),
                 RequestData = new AnonymousTokenRequest
                 {
-                    // TODO: add properties needed in the request
+                    PAsHex = Hex.ToHexString(P.GetEncoded())
                 }
             }, new CancellationToken());
 
@@ -130,9 +172,16 @@ namespace Fhi.Smittestopp.Verification.Tests.Domain.AnonymousTokens
                 result.HasValue.Should().BeTrue();
                 var anonymousTokenReponse = result.ValueOrFailure();
 
-                // TODO: add proper checks
                 anonymousTokenReponse.Should().NotBeNull();
+
+                anonymousTokenReponse.QAsHex.Should().Be(Hex.ToHexString(Q.GetEncoded()));
+                anonymousTokenReponse.ProofCAsHex.Should().Be(Hex.ToHexString(proofC.ToByteArray()));
+                anonymousTokenReponse.ProofZAsHex.Should().Be(Hex.ToHexString(proofZ.ToByteArray()));
             }
+
+            var W = initiator.RandomiseToken(ecParameters, publicKey, P, Q, proofC, proofZ, r);
+            var isVerified = await tokenVerifier.VerifyTokenAsync(privateKey, ecParameters.Curve, t, W);
+            isVerified.Should().BeTrue();
         }
     }
 }

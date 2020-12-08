c9c3299fe849a8c2e411df5d6a02ed3c42923dde
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-12-08 18:07:11 +0100

    Adds properties to response

--- Fhi.Smittestopp.Verification.Domain/Dtos/AnonymousTokenResponse.cs
+++ Fhi.Smittestopp.Verification.Domain/Dtos/AnonymousTokenResponse.cs
@@ -1,7 +1,22 @@
-﻿namespace Fhi.Smittestopp.Verification.Domain.Dtos
+﻿using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace Fhi.Smittestopp.Verification.Domain.Dtos
 {
     public class AnonymousTokenResponse
     {
-        //TODO: add response data for created anonymous token
+        public string QAsHex { get; set; }
+
+        public string ProofCAsHex { get; set; }
+
+        public string ProofZAsHex { get; set; }
+
+        public AnonymousTokenResponse(ECPoint Q, BigInteger proofC, BigInteger proofZ)
+        {
+            QAsHex = Hex.ToHexString(Q.GetEncoded());
+            ProofCAsHex = Hex.ToHexString(proofC.ToByteArray());
+            ProofZAsHex = Hex.ToHexString(proofZ.ToByteArray());
+        }
     }
 }

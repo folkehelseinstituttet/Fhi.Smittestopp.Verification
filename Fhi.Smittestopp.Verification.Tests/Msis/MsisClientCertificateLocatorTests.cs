879dd2edd039a7d5d36c425d4b0cebb61a7985ac
Sindre Møgster Braaten <sindre.mb@gmail.com>
2021-01-06 16:51:54 +0100

    Added tests for msis config and client certificate loading

--- Fhi.Smittestopp.Verification.Tests/Msis/MsisClientCertificateLocatorTests.cs
+++ Fhi.Smittestopp.Verification.Tests/Msis/MsisClientCertificateLocatorTests.cs
@@ -1,9 +1,132 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Fhi.Smittestopp.Verification.Domain.Interfaces;
+using Fhi.Smittestopp.Verification.Msis;
+using Fhi.Smittestopp.Verification.Tests.TestUtils;
+using FluentAssertions;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+using Optional;
 
 namespace Fhi.Smittestopp.Verification.Tests.Msis
 {
     [TestFixture]
     public class MsisClientCertificateLocatorTests
     {
+        [Test]
+        public void GetCertificate_CertNotFound_ThrowsException()
+        {
+            var options = new MsisClientCertLocator.Config
+            {
+                CertId = "key-id"
+            };
+            var cacheKey = nameof(MsisClientCertLocator);
+            object emptyCachedResult = null;
+
+            var automocker = new AutoMocker();
+
+            automocker.SetupOptions(options);
+
+            automocker
+                .Setup<IMemoryCache, bool>(x => x.TryGetValue(cacheKey, out emptyCachedResult))
+                .Returns(false);
+
+            automocker
+                .Setup<IMemoryCache, ICacheEntry>(x => x.CreateEntry(cacheKey))
+                .Returns(Mock.Of<ICacheEntry>());
+
+            automocker
+                .Setup<ICertificateLocator, Option<X509Certificate2>>(x => x.GetCertificate("key-id"))
+                .Returns(Option.None<X509Certificate2>());
+
+            var target = automocker.CreateInstance<MsisClientCertLocator>();
+
+            Assert.Throws<Exception>(() => target.GetCertificate());
+        }
+
+        [Test]
+        public void GetMasterKeyCertificate_NotFoundCertCached_ThrowsException()
+        {
+            var options = new MsisClientCertLocator.Config
+            {
+                CertId = "key-id"
+            };
+            var cacheKey = nameof(MsisClientCertLocator);
+            object cachedResult = Option.None<X509Certificate2>();
+
+            var automocker = new AutoMocker();
+
+            automocker.SetupOptions(options);
+
+            automocker
+                .Setup<IMemoryCache, bool>(x => x.TryGetValue(cacheKey, out cachedResult))
+                .Returns(true);
+
+            var target = automocker.CreateInstance<MsisClientCertLocator>();
+
+            Assert.Throws<Exception>(() => target.GetCertificate());
+        }
+
+        [Test]
+        public void GetMasterKeyCertificate_CertCached_ReturnsFromCache()
+        {
+            var options = new MsisClientCertLocator.Config
+            {
+                CertId = "key-id"
+            };
+            var cacheKey = nameof(MsisClientCertLocator);
+            var certificate = CertUtils.GenerateTestCert();
+            object cachedResult = certificate.Some();
+
+            var automocker = new AutoMocker();
+
+            automocker.SetupOptions(options);
+
+            automocker
+                .Setup<IMemoryCache, bool>(x => x.TryGetValue(cacheKey, out cachedResult))
+                .Returns(true);
+
+            var target = automocker.CreateInstance<MsisClientCertLocator>();
+
+            var result = target.GetCertificate();
+
+            result.Should().Be(certificate);
+        }
+
+        [Test]
+        public void GetMasterKeyCertificate_CertNotCached_ReturnsFromLocator()
+        {
+            var options = new MsisClientCertLocator.Config
+            {
+                CertId = "key-id"
+            };
+            var cacheKey = nameof(MsisClientCertLocator);
+            object emptyCachedResult = null;
+            var certificate = CertUtils.GenerateTestCert();
+
+            var automocker = new AutoMocker();
+
+            automocker.SetupOptions(options);
+
+            automocker
+                .Setup<IMemoryCache, bool>(x => x.TryGetValue(cacheKey, out emptyCachedResult))
+                .Returns(false);
+
+            automocker
+                .Setup<IMemoryCache, ICacheEntry>(x => x.CreateEntry(cacheKey))
+                .Returns(Mock.Of<ICacheEntry>());
+
+            automocker
+                .Setup<ICertificateLocator, Option<X509Certificate2>>(x => x.GetCertificate("key-id"))
+                .Returns(certificate.Some());
+
+            var target = automocker.CreateInstance<MsisClientCertLocator>();
+
+            var result = target.GetCertificate();
+
+            result.Should().Be(certificate);
+        }
     }
 }
---
+++ Fhi.Smittestopp.Verification.Tests/Msis/MsisConfigExtensionsTests.cs
@@ -0,0 +1,70 @@
+﻿using Fhi.Smittestopp.Verification.Domain.Interfaces;
+using Fhi.Smittestopp.Verification.Msis;
+using Fhi.Smittestopp.Verification.Msis.Interfaces;
+using Fhi.Smittestopp.Verification.Tests.TestUtils;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using Optional;
+
+namespace Fhi.Smittestopp.Verification.Tests.Msis
+{
+    [TestFixture]
+    public class MsisConfigExtensionsTests
+    {
+        [Test]
+        public void AddMsisLookup_GivenMockActive_AddsMockClientToCollection()
+        {
+            // Arrange
+            var config = new MsisConfig
+            {
+                Mock = true
+            };
+
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.AddMsisLookup(config);
+
+            // Assert
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var msisClient = serviceProvider.GetRequiredService<IMsisClient>();
+            msisClient.Should().BeOfType<MockMsisClient>();
+        }
+
+        [Test]
+        public void AddMsisLookup_GivenMockDisabled_AddsRealClientToCollection()
+        {
+            // Arrange
+            var config = new MsisConfig
+            {
+                Mock = false,
+                CertId = "some-cert",
+                BaseUrl = "https://msis.api.com/api/v1/"
+            };
+
+            var certificate = CertUtils.GenerateTestCert();
+
+            var certLocator = new Mock<ICertificateLocator>();
+
+            certLocator
+                .Setup(x => x.GetCertificate("some-cert"))
+                .Returns(certificate.Some());
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddMemoryCache();
+            serviceCollection.AddSingleton(certLocator.Object);
+
+            // Act
+            serviceCollection.AddMsisLookup(config);
+
+            // Assert
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var msisClient = serviceProvider.GetRequiredService<IMsisClient>();
+            msisClient.Should().BeOfType<MsisClient>();
+            certLocator.Verify();
+        }
+    }
+}
